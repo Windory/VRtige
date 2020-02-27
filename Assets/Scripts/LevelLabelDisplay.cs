cc7727ba036947a0af10c946697be55a1cc03e3a
C-Leroux <corentin_leroux@outlook.fr>
2020-02-27 19:32:22 +0100

    Interface graphique : ajout de l'ecran de description de niveau

---
+++ Assets/Scripts/LevelLabelDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelLabelDisplay : MonoBehaviour
+{
+    public Text labelText;
+    private int lvl = 1;   // Entier à modifier selon le niveau sélectionné
+
+    // Update is called once per frame
+    void Update()
+    {
+        labelText.text = "Niveau " + lvl;
+    }
+}
