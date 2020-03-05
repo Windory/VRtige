0b05d11d90a9ab84b407e7f0902ab337e00dea76
C-Leroux <corentin_leroux@outlook.fr>
2020-03-05 15:30:25 +0100

    interface : ajout animation

---
+++ Assets/Scripts/ButtonAnim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAnim : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        anim.SetTrigger("IsOver");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        anim.SetTrigger("IsOver");
+    }
+}
