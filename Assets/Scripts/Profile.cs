098037989ae6ff34b4382f6b1b5c6d35c27e8907
Wind <wind.91@hotmail.fr>
2020-04-19 13:31:48 +0200

    interface register ajoutée

--- Assets/Scripts/Profile.cs
+++ Assets/Scripts/Profile.cs
@@ -1,13 +1,26 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Text;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using System;
+
 namespace test2logpa8
 {
-    class Profile
+    class Profile : MonoBehaviour
     {
+        public GameObject loginScreen;
+        public GameObject registerScreen;
+
+        public Text loginData;
+        public Text pswData;
+        public Text confirmPswData;
+        public Text nomData;
+        public Text prenomData;
+        public Text errorLabel;
+
         public bool create_profile(string nom, string prenom, string pseudo, string pwd, string numsuiv, string therapeute)
         {
             try
@@ -32,5 +45,31 @@ namespace test2logpa8
                 return false;
             }
         }
+
+        public void SendProfile()
+        {
+            string nom = nomData.text;
+            string prenom = prenomData.text;
+            string pseudo = loginData.text;
+            string pwd = pswData.text;
+            string confirmPwd = confirmPswData.text;
+
+            if (pwd != confirmPwd)
+            {
+                errorLabel.text = "Erreur : les mots de passe ne correspondent pas";
+            }
+            else
+            {
+                if (create_profile(nom, prenom, pseudo, pwd, "0", "null"))
+                {
+                    loginScreen.SetActive(true);
+                    registerScreen.SetActive(false);
+                }
+                else
+                {
+                    errorLabel.text = "Erreur : le profile n'a pas pu être créé";
+                }
+            }
+        }
     }
-}*/
+}
